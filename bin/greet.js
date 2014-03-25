68b73e57ef3e46a83295319883b601b18c01ae77
easier <fysian@gmail.com>
2014-03-25 18:18:01 +0800

    greet-drunk

--- bin/greet.js
+++ bin/greet.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 //console.log("Hello,world")
-var greet = require("../index.js")
+var parseArgs = require('minimist')
+var greet = require("greet")
 var name = process.argv[2]
-console.log(greet(name));
+var argv = require('minimist')(process.argv.slice(2));
+console.log(greet(name,argv.drunk));
